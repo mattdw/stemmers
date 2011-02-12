cbf366266134b92c72e3937769d92ba457155cff
Matt Wilson <matt@culturethree.com>
2011-02-12 14:16:14 +1300

    Add github url to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject stemmers "0.2.0"
   :description "A collection of stemmers for use in search engines and the like."
+  :url "http://github.com/mattdw/stemmers"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[marginalia "0.3.2"]
