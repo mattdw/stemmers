7a29b412352ebb604058b357ba332a8b53d0565f
Matt Wilson <matt@culturethree.com>
2014-10-27 11:53:18 +1300

    bump version to 0.2.2

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject stemmers "0.2.1"
+(defproject stemmers "0.2.2"
   :description "A collection of stemmers for use in search engines and the like."
+  :url "http://github.com/mattdw/stemmers"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :dev-dependencies [[marginalia "0.3.2"]
                      [swank-clojure "1.3.0-SNAPSHOT"]])
