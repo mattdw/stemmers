3e9dab6bc880867c61b127dc4d38a622633a5f13
Matt Wilson <matt@culturethree.com>
2023-01-17 17:29:31 +1300

    bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject stemmers "0.2.2"
+(defproject stemmers "0.2.3"
   :description "A collection of stemmers for use in search engines and the like."
   :url "http://github.com/mattdw/stemmers"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :dev-dependencies [[marginalia "0.3.2"]
                      [swank-clojure "1.3.0-SNAPSHOT"]])
